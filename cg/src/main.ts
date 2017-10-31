4e14cc55565f2d1ae9632b103fa407961511c1de
wangb <wangbinyq@gmail.com>
2017-10-31 14:25:20 +0800

    add perspective

--- cg/src/main.ts
+++ cg/src/main.ts
@@ -1,4 +1,5 @@
 import { setupContext } from './utils'
+import { mat4, glMatrix } from 'gl-matrix'
 import Shader from './shader'
 import vs from './shaders/vs'
 import fs from './shaders/fs'
@@ -8,16 +9,60 @@ const shader = new Shader(gl, vs, fs)
 shader.use()
 
 const vertices = [
-    // positions          // colors           // texture coords
-  0.5,  0.5, 0.0,   1.0, 0.0, 0.0,   1.0, 1.0,   // top right
-  0.5, -0.5, 0.0,   0.0, 1.0, 0.0,   1.0, 0.0,   // bottom right
-  -0.5, -0.5, 0.0,   0.0, 0.0, 1.0,   0.0, 0.0,   // bottom left
-  -0.5,  0.5, 0.0,   1.0, 1.0, 0.0,   0.0, 1.0    // top left
+  -0.5, -0.5, -0.5,  0.0, 0.0,
+  0.5, -0.5, -0.5,  1.0, 0.0,
+  0.5,  0.5, -0.5,  1.0, 1.0,
+  0.5,  0.5, -0.5,  1.0, 1.0,
+  -0.5,  0.5, -0.5,  0.0, 1.0,
+  -0.5, -0.5, -0.5,  0.0, 0.0,
+
+  -0.5, -0.5,  0.5,  0.0, 0.0,
+  0.5, -0.5,  0.5,  1.0, 0.0,
+  0.5,  0.5,  0.5,  1.0, 1.0,
+  0.5,  0.5,  0.5,  1.0, 1.0,
+  -0.5,  0.5,  0.5,  0.0, 1.0,
+  -0.5, -0.5,  0.5,  0.0, 0.0,
+
+  -0.5,  0.5,  0.5,  1.0, 0.0,
+  -0.5,  0.5, -0.5,  1.0, 1.0,
+  -0.5, -0.5, -0.5,  0.0, 1.0,
+  -0.5, -0.5, -0.5,  0.0, 1.0,
+  -0.5, -0.5,  0.5,  0.0, 0.0,
+  -0.5,  0.5,  0.5,  1.0, 0.0,
+
+  0.5,  0.5,  0.5,  1.0, 0.0,
+  0.5,  0.5, -0.5,  1.0, 1.0,
+  0.5, -0.5, -0.5,  0.0, 1.0,
+  0.5, -0.5, -0.5,  0.0, 1.0,
+  0.5, -0.5,  0.5,  0.0, 0.0,
+  0.5,  0.5,  0.5,  1.0, 0.0,
+
+  -0.5, -0.5, -0.5,  0.0, 1.0,
+  0.5, -0.5, -0.5,  1.0, 1.0,
+  0.5, -0.5,  0.5,  1.0, 0.0,
+  0.5, -0.5,  0.5,  1.0, 0.0,
+  -0.5, -0.5,  0.5,  0.0, 0.0,
+  -0.5, -0.5, -0.5,  0.0, 1.0,
+
+  -0.5,  0.5, -0.5,  0.0, 1.0,
+  0.5,  0.5, -0.5,  1.0, 1.0,
+  0.5,  0.5,  0.5,  1.0, 0.0,
+  0.5,  0.5,  0.5,  1.0, 0.0,
+  -0.5,  0.5,  0.5,  0.0, 0.0,
+  -0.5,  0.5, -0.5,  0.0, 1.0
 ]
 
-const indices = [
-  0, 1, 3,
-  1, 2, 3
+const cubePosition = [
+  [0.0,  0.0,  0.0],
+  [2.0,  5.0, -15.0],
+  [-1.5, -2.2, -2.5],
+  [-3.8, -2.0, -12.3],
+  [2.4, -0.4, -3.5],
+  [-1.7,  3.0, -7.5],
+  [1.3, -2.0, -2.5],
+  [1.5,  2.0, -2.5],
+  [1.5,  0.2, -1.5],
+  [-1.3,  1.0, -1.5]
 ]
 
 const texCoords = [
@@ -28,26 +73,23 @@ const texCoords = [
 
 const vbo = gl.createBuffer()
 const vao = gl.createVertexArray()
-const ebo = gl.createBuffer()
 gl.bindVertexArray(vao)
 
 gl.bindBuffer(gl.ARRAY_BUFFER, vbo)
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW)
 
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ebo)
-gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int32Array(indices), gl.STATIC_DRAW)
-
-gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 8 * Float32Array.BYTES_PER_ELEMENT, 0)
+gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 0)
 gl.enableVertexAttribArray(0)
 
-gl.vertexAttribPointer(1, 3, gl.FLOAT, false, 8 * Float32Array.BYTES_PER_ELEMENT, 3 * Float32Array.BYTES_PER_ELEMENT)
+gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 3 * Float32Array.BYTES_PER_ELEMENT)
 gl.enableVertexAttribArray(1)
 
-gl.vertexAttribPointer(2, 2, gl.FLOAT, false, 8 * Float32Array.BYTES_PER_ELEMENT, 6 * Float32Array.BYTES_PER_ELEMENT)
-gl.enableVertexAttribArray(2)
+// gl.vertexAttribPointer(2, 2, gl.FLOAT, false, 8 * Float32Array.BYTES_PER_ELEMENT, 6 * Float32Array.BYTES_PER_ELEMENT)
+// gl.enableVertexAttribArray(2)
 
 gl.clearColor(0.2, 0.3, 0.3, 1)
-gl.clear(gl.COLOR_BUFFER_BIT)
+gl.enable(gl.DEPTH_TEST)
+gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
 const texture = gl.createTexture()
 gl.bindTexture(gl.TEXTURE_2D, texture)
@@ -65,15 +107,72 @@ const height = image.naturalHeight
 
 gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, width, height, 0, gl.RGB, gl.UNSIGNED_BYTE, image)
 
+let viewPositionX = 0
+let viewPositionY = 0
+let viewPositionZ = -3
+let model
+let view
+let projection
+
 function render () {
-  gl.clear(gl.COLOR_BUFFER_BIT)
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
   // const time = Date.now() / 1000
   // const position = Math.sin(time)
   // shader.setFloat('position', position)
 
-  gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, 0)
+  model = mat4.fromXRotation(mat4.create(), glMatrix.toRadian(-55))
+  view = mat4.fromTranslation(mat4.create(), [viewPositionX, viewPositionY, viewPositionZ])
+  projection = mat4.perspective(mat4.create(), glMatrix.toRadian(45), width / height, 0.1, 100)
+
+  // model = mat4.rotate(model, model, Date.now() / 1000 * glMatrix.toRadian(50), [0.5, 1, 0])
+  // shader.setMatrix4fv('model', model)
+  shader.setMatrix4fv('view', view)
+  shader.setMatrix4fv('projection', projection)
+  for (let i = 0; i < 10; i++) {
+    model = mat4.translate(model, model, cubePosition[i])
+    const angle = 20 * i
+    // model = mat4.rotate(model, model, glMatrix.toRadian(angle), [1, 0.3, 0.5])
+    shader.setMatrix4fv('model', model)
+
+    gl.drawArrays(gl.TRIANGLES, 0, vertices.length / 5)
+  }
   requestAnimationFrame(render)
 }
 
+function setupUi () {
+  const $canvas = document.getElementById('gl-canvas')
+  const $far = document.getElementById('far')
+  const $near = document.getElementById('near')
+  const $left = document.getElementById('left')
+  const $up = document.getElementById('up')
+  const $right = document.getElementById('right')
+  const $down = document.getElementById('down')
+
+  $far.onclick = () => viewPositionZ += 0.1
+  $near.onclick = () => viewPositionZ -= 0.1
+  $left.onclick = () => viewPositionX -= 0.1
+  $up.onclick = () => viewPositionY += 0.1
+  $right.onclick = () => viewPositionX += 0.1
+  $down.onclick = () => viewPositionY -= 0.1
+
+  window.onkeydown = (e) => {
+    switch (e.keyCode) {
+      case 37:
+        viewPositionX -= 0.01
+        break
+      case 38:
+        viewPositionY += 0.01
+        break
+      case 39:
+        viewPositionX += 0.01
+        break
+      case 40:
+        viewPositionY -= 0.01
+        break
+    }
+  }
+}
+
+setupUi()
 render()
--- cg/src/shader.ts
+++ cg/src/shader.ts
@@ -29,4 +29,8 @@ export default class Shader {
   setFloat (name: string, value: number) {
     this.gl.uniform1f(this.getUniformLocation(name), value)
   }
+
+  setMatrix4fv (name: string, value: Float32Array | [number]) {
+    this.gl.uniformMatrix4fv(this.getUniformLocation(name), false, value)
+  }
 }
