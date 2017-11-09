dbcc0a37f25b69c0289849dfd06b7dca7e81a417
wangb <wangbinyq@gmail.com>
2017-11-09 20:58:23 +0800

    update ignore

--- raytracing-oneweek/vec3.h
+++ raytracing-oneweek/vec3.h
@@ -3,6 +3,7 @@
 #include <iostream>
 
 class vec3 {
+  using namespace std;
 public:
   vec3() {}
   vec3(float e0, float e1, float e2) {
@@ -24,10 +25,78 @@ public:
   inline float& operator[](int i) { return e[i]; }
 
   inline vec3& operator+=(const vec3 &v2);
-  inline vec3& operator+=(const vec3 &v2);
-  inline vec3& operator+=(const vec3 &v2);
-  inline vec3& operator+=(const vec3 &v2);
-  inline vec3& operator+=(const vec3 &v2);
+  inline vec3& operator-=(const vec3 &v2);
+  inline vec3& operator*=(const vec3 &v2);
+  inline vec3& operator/=(const vec3 &v2);
+  inline vec3& operator*=(const float t);
+  inline vec3& operator/=(const float t);
 
+  inline float length() const {
+    return sqrt(squared_length());
+  }
+  inline float squared_length() const {
+    return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+  }
+  inline void make_unit_vector();
+
+  
+public:
   float e[3];
+}
+
+inline istream& operator>>(istream &is, vec3 &t) {
+  is >> t.e[0] >> t.e[1] >> t.e[2];
+  return is;
+}
+
+inline ostream& operator<<(ostream &os, const vec3 &t) {
+  os << t.e[0] << t.e[1] << t.e[2];
+  return os;
+}
+
+inline void vec3::make_unit_vector() {
+  float k = 1.0 / length();
+  e[0] *= k;
+  e[1] *= k;
+  e[2] *= k;
+}
+
+inline vec3 operator+(const vec3 &v1, const vec3 &v2) {
+  return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
+}
+
+inline vec3 operator-(const vec3 &v1, const vec3 &v2) {
+  return vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2]);
+}
+
+inline vec3 operator*(const vec3 &v1, const vec3 &v2) {
+  return vec3(v1.e[0] * v2.e[0], v1.e[1] * v2.e[1], v1.e[2] * v2.e[2]);
+}
+
+inline vec3 operator/(const vec3 &v1, const vec3 &v2) {
+  return vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2]);
+}
+
+inline vec3 operator*(float t, const vec3 &v) {
+  return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+inline vec3 operator/(const vec3 &v, float t) {
+  return vec3(v.e[0] / t, v.e[1] / t, v.e[2] / t);
+}
+
+inline vec3 operator*(const vec3 &v, float t) {
+  return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+inline float dot(const vec3 &v1, const vec3 &v2) {
+  return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v2.e[2];
+}
+
+inline vec3 cross(const vec3 &v1, const vec3 &v2) {
+  return vec3(
+    v1.e[1] * v2.e[2] - v1.e[2] * v2.e[1],
+    -(v1.e[0] * v2.e[2] - v1.e[2] * v2.e[0]),
+    v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0]
+  )
 }
