18ed3781ab14df4a276c97405002423e2682f6b5
wangb <wangbinyq@gmail.com>
2017-11-10 10:39:34 +0800

    color ray

--- raytracing-oneweek/main.cpp
+++ raytracing-oneweek/main.cpp
@@ -2,6 +2,12 @@
 #include "vec3.h"
 #include "ray.h"
 
+vec3 color(const ray &r) {
+  vec3 unit_direction = unit_vector(r.direction());
+  float t = 0.5 * (unit_direction.y() + 1.0);
+  return (1.0 - t) * vec3(1, 1, 1) + t * vec3(0.5, 0.7, 1.0);
+}
+
 int main () {
   using namespace std;
 
@@ -10,15 +16,22 @@ int main () {
   
   cout << "P3\n" << nx << " " << ny << "\n255\n";
 
+  vec3 lower_left_corner(-2.0, -1.0, -1.0);
+  vec3 horizontal(4.0, 0.0, 0.0);
+  vec3 vertical(0.0, 2.0, 0.0);
+  vec3 origin(0.0, 0.0, 0.0);
+
   for (int j = ny -1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
-      float r = float(i) / float(nx);
-      float g = float(j) / float(ny);
-      float b = 0.2;
+      float u = float(i) / float(nx);
+      float v = float(j) / float(ny);
 
-      int ir = int(255.99 * r);
-      int ig = int(255.99 * g);
-      int ib = int(255.99 * b);
+      ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+      vec3 col = color(r);
+
+      int ir = int(255.99 * col[0]);
+      int ig = int(255.99 * col[1]);
+      int ib = int(255.99 * col[2]);
       cout << ir << " " << ig << " " << ib << "\n";
     }
   }
--- raytracing-oneweek/ray.h
+++ raytracing-oneweek/ray.h
@@ -8,7 +8,7 @@ public:
   ray () {}
   ray (const vec3 &a, const vec3 &b): A(a), B(b) {}
   vec3 origin() const { return A; }
-  vec3 direction() const { return A; }
+  vec3 direction() const { return B; }
   vec3 point_at_parameter(float t) const { return A + t*B; }
 
 public:
