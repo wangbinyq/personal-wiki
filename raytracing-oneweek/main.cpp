8d7e3066b642aef1e1c886ce44aa8b2e20283f66
wangb <wangbinyq@gmail.com>
2017-11-10 18:54:06 +0800

    change camer position

--- raytracing-oneweek/main.cpp
+++ raytracing-oneweek/main.cpp
@@ -39,7 +39,7 @@ int main () {
   list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
   
   hitable* world = new hitable_list(list, 5);
-  camera cam;
+  camera cam(vec3(-2, 1, 1), vec3(0, 0, -1), vec3(0, 1, 0), 90, float(nx) / float(ny));
 
   for (int j = ny -1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
