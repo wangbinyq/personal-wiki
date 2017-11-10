987fd0f1ec3f294346eee6f2809c02b8b0021b7f
wangb <wangbinyq@gmail.com>
2017-11-10 23:20:18 +0800

    add random scene

--- raytracing-oneweek/main.cpp
+++ raytracing-oneweek/main.cpp
@@ -22,6 +22,37 @@ vec3 color(const ray &r, hitable* world, int depth = 0) {
   }
 }
 
+hitable* random_scene() {
+    int n = 500;
+    hitable **list = new hitable*[n+1];
+    list[0] =  new sphere(vec3(0,-1000,0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+    int i = 1;
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            float choose_mat = drand48();
+            vec3 center(a+0.9*drand48(),0.2,b+0.9*drand48()); 
+            if ((center-vec3(4,0.2,0)).length() > 0.9) { 
+                if (choose_mat < 0.8) {  // diffuse
+                    list[i++] = new sphere(center, 0.2, new lambertian(vec3(drand48()*drand48(), drand48()*drand48(), drand48()*drand48())));
+                }
+                else if (choose_mat < 0.95) { // metal
+                    list[i++] = new sphere(center, 0.2,
+                            new metal(vec3(0.5*(1 + drand48()), 0.5*(1 + drand48()), 0.5*(1 + drand48())),  0.5*drand48()));
+                }
+                else {  // glass
+                    list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+                }
+            }
+        }
+    }
+
+    list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+    list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+    list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+    return new hitable_list(list,i);
+}
+
 int main () {
   using namespace std;
 
@@ -39,13 +70,21 @@ int main () {
   list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
   
   hitable* world = new hitable_list(list, 5);
-  camera cam(vec3(-2, 1, 1), vec3(0, 0, -1), vec3(0, 1, 0), 90, float(nx) / float(ny));
+  world = random_scene();
+
+  vec3 lookfrom(13, 2, 3);
+  vec3 lookat(0, 0, 0);
+  float dist_focus = 10;
+  float aperture = 0.1;
+
+  camera cam(lookfrom, lookat, vec3(0, 1, 0),
+    20, float(nx) / float(ny), aperture, dist_focus);
 
   for (int j = ny -1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
       vec3 col(0, 0, 0);
 
-      for(int s=0; s < ns; s++) {
+      for(int s = 0; s < ns; s++) {
         float u = float(i + drand48()) / float(nx);
         float v = float(j + drand48()) / float(ny);
 
